7e032c3c34c4acd6e11ffa198262b8927dc2e0f2
nickhitt <73009988+nickhitt@users.noreply.github.com>
2022-06-03 17:23:15 +1200

    data load

---
+++ load_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+enroll_states = pd.read_csv("enroll_states.csv")
+finance_districts = pd.read_csv("finance_districts.csv")
+finance_states = pd.read_csv("finance_states.csv")
+naep_states = pd.read_csv("naep_states.csv")
+states_all = pd.read_csv("states_all.csv")
