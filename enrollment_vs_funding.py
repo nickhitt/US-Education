5cb996361a2939a5f0b10b8fe959610ade74c5e8
nickhitt <73009988+nickhitt@users.noreply.github.com>
2022-06-03 21:32:38 +1200

    data load

--- enrollment_vs_funding.py
+++ enrollment_vs_funding.py
@@ -1,13 +1,20 @@
+import pandas as pd
+
 exec(open("load_data.py").read())
 
 import seaborn as sns
 import numpy as np
-
 import statsmodels.api as sm
 
 states = states_all["STATE"].unique()
+state_list = []
+slope_list = []
+for state in states:
+    df = states_all[states_all["STATE"] == state][["ENROLL",
+                                                   "TOTAL_EXPENDITURE"]].dropna(thresh=2)
+    model = sm.OLS(df["ENROLL"], df["TOTAL_EXPENDITURE"]).fit()
+    slope_list.append(model.params)
+    state_list.append(state)
 
-for state in states
-    df = states_all[states_all["STATE"] == state]
-    model = sm.OLS(df["GRADES_ALL_G"], df["TOTAL_EXPENDITURE"])
-
+slopes = pd.DataFrame(slope_list)
+exp_v_enroll = pd.DataFrame([state_list, slopes])
